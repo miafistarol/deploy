9128ad2628205c093b5c30bb489cc2187523f457
miafistarol <miafistarol@gmail.com>
2026-06-17 12:39:35 -0300

    se creo conexion a supabase

--- index.mjs
+++ index.mjs
@@ -1,11 +1,20 @@
+import 'dotenv/config'
 import express, { request } from "express"
 import { Pool } from "pg"
 
+const pool =({
+    host: process.env.BD_HOST,
+    user: process.env.BD_USER,
+    database: process.env.BD_NAME,
+    port: process.env.BD_PORT,
+    pass: process.env.BD_PASS
 
+})
 
 const PUERTO = process.env.PORT || 3000
 const app = express()
-app.get('/test' , (req, res)=> {
+app.get('/test' , async (req, res)=> {
+    await pool.query('SELECT * from mensajes')
     res.send('Test okey')
 })
 
